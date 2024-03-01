7b0fb405c4e99c89c7256cf60c8576fac3506446
Th1nkl1ng <nj-nate@live.co.uk>
2024-03-01 13:10:44 +0000

    2nd Door functionality
    Slight change to the cube collision script file so that the second door has the opposite behavior to the 1st door. This develops the puzzle to complete the second room.

--- Assets/Scripts/interactable/Cube Collision.cs
+++ Assets/Scripts/interactable/Cube Collision.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class CubeCollision : MonoBehaviour
 {
+    // door 1 collision
+    [Header("Door 1 collision")]
     [SerializeField]
     GameObject DoorPrefab;
     [SerializeField]
@@ -11,11 +13,22 @@ public class CubeCollision : MonoBehaviour
     [SerializeField]
     BoxCollider OffArea;
     [SerializeField]
-    int doorx;
+    int door1x;
     [SerializeField]
-    int doory;
+    int door1y;
     [SerializeField]
-    int doorz;
+    int door1z;
+
+    //door 2 collision
+    [Header("Door 2 collision")]
+    [SerializeField]
+    GameObject DoorPrefab2;
+    [SerializeField]
+    float door2x;
+    [SerializeField]
+    int door2y;
+    [SerializeField]
+    int door2z;
 
     int doorTranslate  = 6;
 
@@ -37,18 +50,30 @@ public class CubeCollision : MonoBehaviour
         if (col.gameObject == CollisionActor)
         {
             //Debug.Log(" TEST ");
+            //door 1 go up
             DoorPrefab.transform.position += new Vector3(0, doorTranslate, 0);
+            // door 2 stays where it is
+            DoorPrefab2.transform.position = new Vector3(door2x, door2y, door2z);
         }
         else 
         {
-            DoorPrefab.transform.position = new Vector3(-9, 3, 81);
+            //door 1 stays where it is / goes back to where it was
+            DoorPrefab.transform.position = new Vector3(door1x, door1y, door1z);
+            // Door 2 moves up
+            DoorPrefab2.transform.position += new Vector3(0, doorTranslate, 0);
         }
+
+
+        
     }
 
     private void OnTriggerEnter(Collider collider)
     {
         collider = OffArea;
         //Debug.Log(" TEST 2 ");
-        DoorPrefab.transform.position = new Vector3(doorx, doory, doorz);
+        //door 1 stays where it is / goes back to where it was
+        DoorPrefab.transform.position = new Vector3(door1x, door1y, door1z);
+        // Door 2 moves up
+        DoorPrefab2.transform.position += new Vector3(0, doorTranslate, 0);
     }
 }
