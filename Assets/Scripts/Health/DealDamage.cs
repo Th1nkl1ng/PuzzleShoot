b537e547b77c7535f8374f818358128d685787a5
Th1nkl1ng <nj-nate@live.co.uk>
2024-04-08 17:45:47 +0100

    UI and change between levels
    Added a basic UI which can be changed based on a Health variable.
    
    Testing for loading the next level of the game.

---
+++ Assets/Scripts/Health/DealDamage.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DealDamage : MonoBehaviour 
+{
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            SendDamage(10);
+        }
+    }
+
+
+    public void SendDamage (int dam)
+	{
+		PlayerHealth playerStats = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerHealth>();
+		playerStats.TakeDamage(dam);
+	}
+}
---
+++ Assets/Scripts/Health/PlayerHealth.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class PlayerHealth : MonoBehaviour 
+{
+	public float health;
+	public TMP_Text healthText;
+	public float MaxHealth = 100f;
+
+    public void Start()
+    {
+		health = MaxHealth;
+    }
+    public void TakeDamage(int damage)
+	{
+		health -= damage;
+		//Debug.Log("Health = " + health.ToString());
+	}
+
+    private void Update()
+    {
+		healthText.text = health.ToString();
+		health = Mathf.Clamp(health, 0, MaxHealth);
+    }
+}
